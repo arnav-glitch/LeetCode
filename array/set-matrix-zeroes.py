b9dc4bfe7e48ba613d4316695a70528d148d0de0
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-08-16 18:22:28 +0530

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

--- array/set-matrix-zeroes.py
+++ array/set-matrix-zeroes.py
@@ -18,10 +18,10 @@ class Solution:
                     matrix[i][j] = 0
 
         if matrix[0][0] == 0:
-            for i in range(len(matrix)):
-                matrix[i][0] = 0
-        
-        if col0 == 0:
             for j in range(len(matrix[i])):
                 matrix[0][j] = 0
+        
+        if col0 == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
         

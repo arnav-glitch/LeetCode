242cb600f56bbd420072733fbb2f4bb9f55fb247
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-08-18 14:34:16 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
+        result = []
         for i in range(len(nums)):
             difference = target - nums[i]
-            if difference in seen:
-                return [seen[difference], i]
-            else:
+            if difference not in seen:
                 seen[nums[i]] = i
-    
+            else:
+                return [seen[difference], i]
         

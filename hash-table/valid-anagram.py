9c714a0cf569af08187be891e43600dfec9af54a
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-08-18 14:42:36 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,8 +1,11 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter = Counter(t)
+        counter1 = Counter(t)
+        counter2 = Counter(s)
         for i in s:
-            if i not in counter:
+            if i not in counter1:
+                return False
+            if i not in counter2:
                 return False
         return True

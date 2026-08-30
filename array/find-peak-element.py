3c1f472f0ecf3411f46e7e7d0017f6925fdb38ea
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-08-31 00:27:01 +0530

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

---
+++ array/find-peak-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] < nums[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l

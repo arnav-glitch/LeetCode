62effd35796ffda010e9e0184731f93eb9bd4ba1
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-09-19 00:57:07 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,16 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        hm = {}
-        for i in s:
-            hm[i] = hm.get(i, 0) + 1
-        for i in t:
-            if i not in hm:
+        s_map = {}
+        for ch in s:
+            s_map[ch] = s_map.get(ch, 0) + 1
+        t_map = {}
+        for ch in t:
+            t_map[ch] = t_map.get(ch, 0) + 1
+        for key, value in t_map.items():
+            if key not in s_map:
                 return False
             else:
-                hm[i] -= 1
-        for i in hm:
-            if hm[i] != 0:
-                return False
+                s_value = s_map.get(key, 0)
+                t_value = t_map.get(key, 0)
+                if s_value != t_value:
+                    return False
         return True

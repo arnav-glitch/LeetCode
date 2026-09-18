2a21dbe0bd7b31afdc0c8d0a8b83cd96a95f2c89
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-09-18 23:31:53 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

---
+++ array/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+            else:
+                continue
+        return False

5a9601a30bfc153510c1571fbd38c6e922d1cdfc
arnav-glitch <65273513+arnav-glitch@users.noreply.github.com>
2026-09-19 00:50:55 +0530

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- hash-table/top-k-frequent-elements.py
+++ hash-table/top-k-frequent-elements.py
@@ -1,13 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        hm = {}
-        for i in range(len(nums)):
-            hm[nums[i]] = hm.get(nums[i], 0) + 1
-        sorted_hm = dict(sorted(hm.items(), key=lambda x: x[1], reverse = True))
-        result = []
-        for key, value in sorted_hm.items():
-            result.append(key)
-            k -= 1
-            if k == 0:
-                break
-        return result
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        sort = list(sorted(freq, key=lambda num: freq[num], reverse=True))
+        return sort[:k]
